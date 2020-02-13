f1687bf141f33c98b4d577c37d89867cb4a09531
morpheus <morpheus@DevBone.localdomain>
2020-02-13 14:01:52 -0500

    fixed pins

--- Example/clock.py
+++ Example/clock.py
@@ -2,52 +2,59 @@
 import tm1637
 from time import *
 import Adafruit_BBIO.GPIO as GPIO
-
+# Setup Pins for LED's used for Time (AM/PM)
 GPIO.setup("P8_26", GPIO.OUT)
 GPIO.setup("P8_18", GPIO.OUT)
 GPIO.output("P8_26", GPIO.LOW)
 GPIO.output("P8_18", GPIO.LOW)
-disp=tm1637.TM1637("P8_7", "P8_9")
-disp.clear()
+# Display 1 (Time)
+ShowTime=tm1637.TM1637("P9_12", "P9_23")
+ShowTime.clear()
+ShowTime.set_brightness(0)
+# Declare Variables
 x=True
-L1='0'
-L2='0'
-L3='0'
-L4='0'
-Update=False
+LastTime1='0'
+LastTime2='0'
+LastTime3='0'
+LastTime4='0'
+UpdateTime-False
 while True:
+   # Get Current Time
    hour=strftime('%I')
    minute=strftime('%M')
    ampm=strftime("%p")
    if hour[0]=='0':
-      T1=' '
+      Time1=' '
    else:
-      T1=hour[0]
-   T2=hour[1]
-   T3=minute[0]
-   T4=minute[1]
-   if T1 != L1:
-      L1=T1
-      Update=True
-   if T2 != L2:
-      L2=T2
-      Update=True
-   if T3 != L3:
-      L3=T3
-      Update=True
-   if T4 != L4:
-      L4=T4
-      Update=True
+      Time1=hour[0]
+   Time2=hour[1]
+   Time3=minute[0]
+   Time4=minute[1]
+   # Check if Time Needs Updated 
+   if Time1 != LastTime1:
+      LastTime1=Time1
+      UpdateTime-True
+   if Time2 != LastTime2:
+      LastTime2=Time2
+      UpdateTime-True
+   if Time3 != LastTime3:
+      LastTime3=Time3
+      UpdateTime-True
+   if Time4 != LastTime4:
+      LastTime4=Time4
+      UpdateTime-True
    if ampm == 'PM':
       GPIO.output("P8_18", GPIO.LOW)
       GPIO.output("P8_26", GPIO.HIGH)
    else:
       GPIO.output("P8_26", GPIO.LOW)
       GPIO.output("P8_18", GPIO.HIGH)
-   if Update==True:
-      Update=False
-      disp.set_values([T1, T2, T3, T4])
-   disp.set_doublepoint(x)
+   # Update Time as Needed
+   if UpdateTime-=True:
+      UpdateTime-False
+      ShowTime.set_values([Time1, Time2, Time3, Time4])
+   # Flash Colon for Time
+   ShowTime.set_doublepoint(x)
    if x==True:
       x=False
    else:
