3aabdf08f32c0899171a39e055e315b3caede20a
morpheus <morpheus@DevBone.localdomain>
2020-02-13 13:11:18 -0500

    clean up

--- Example/test.py
+++ Example/test.py
@@ -1,19 +1,53 @@
 import tm1637
-from time import *
 
-disp = tm1637.TM1637("P8_7", "P8_9")
-disp.clear()
+disp = tm1637.TM1637(3, 2)
+
+disp.set_values(['A', 'B', 'b', 'C'])
+r = raw_input()
+
+disp.set_values(['c', 'D', 'd', 'E'])
+r = raw_input()
+
+disp.set_values(['F', 'G', 'H', 'h'])
+r = raw_input()
+
+disp.set_values(['I', 'J', 'K', 'L'])
+r = raw_input()
+
+disp.set_values(['l', 'n', 'O', 'o'])
+r = raw_input()
+
+disp.set_values(['P', 'r', 'S', 'U'])
+r = raw_input()
+
+disp.set_values(['Y', 'Z', ' ', ' '])
+r = raw_input()
+
+disp.set_values(['T1', 'T2', 'W1', 'W2'])
+r = raw_input()
+
+disp.set_value('M1', 0)
+r = raw_input()
+
+disp.set_value('M2', 1)
+r = raw_input()
+
+disp.set_values(range(4))
+r = raw_input()
+
+disp.set_values(range(4, 8))
+r = raw_input()
+
+disp.set_values(range(6, 10))
+r = raw_input()
+
 x = True
-while True:
-   hour = strftime('%I')
-   T1=hour[0]
-   T2=hour[1]
-   minute = strftime('%M')
-   T3=minute[0]
-   T4=minute[1]
-   disp.set_values([T1, T2, T3, T4])
-   disp.set_doublepoint(x)
-   if x == True:
-      x = False
-   else:
-      x = True
+for i in range(8):
+    disp.set_doublepoint(x)
+    disp.set_brightness(i)
+    r = raw_input()
+    x = not x
+
+disp.clear()
+
+disp.cleanup()
